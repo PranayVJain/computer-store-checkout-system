a78f76163cb2c4c31af94a68f2aa1563a31164db
Pranay Jain <Pranay.Jain@nuance.com>
2023-03-13 17:17:16 +0530

    Removed unused comments

--- src/test/java/CheckoutTests.java
+++ src/test/java/CheckoutTests.java
@@ -1,19 +1,3 @@
-/*
- * #%L
- * computer-store-checkout-system
- *
- * %%
- * Copyright (C) 2018 - 2022 Nuance Communications Inc. All Rights Reserved.
- * %%
- *
- * The copyright to the computer program(s) herein is the property of
- * Nuance Communications Inc. The program(s) may be used and/or copied
- * only with the written permission from Nuance Communications Inc.
- * or in accordance with the terms and conditions stipulated in the
- * agreement/contract under which the program(s) have been supplied.
- * #L%
- */
-
 import com.cs.computerstore.Checkout;
 import com.cs.computerstore.pricing.PricingRule;
 import java.math.BigDecimal;
